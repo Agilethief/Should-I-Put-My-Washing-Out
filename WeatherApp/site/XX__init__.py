d513b78a4c58873c247b0c6769e53f11e247c876
Agilethief <tbermanseder@gmail.com>
2023-06-05 20:52:42 +1000

    Updated urls

---
+++ WeatherApp/site/XX__init__.py
@@ -0,0 +1,8 @@
+# This is a package
+# Great success
+
+# import app
+
+
+# def getApp():
+#    return app
--- WeatherApp/site/__init__.py
+++
@@ -1,8 +0,0 @@
-# This is a package
-# Great success
-
-import app
-
-
-def getApp():
-    return app
